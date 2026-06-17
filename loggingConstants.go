ea8f1129b7c89127b2f6ba0630370649817e56ae
antisignal <alexandra.minetree.dill@gmail.com>
2026-06-17 17:35:44 -0500

    add events and reasons as constants

--- loggingConstants.go
+++ loggingConstants.go
@@ -1,21 +1,41 @@
 package main
 
 const (
-	LOG_LEVEL_DEBUG = iota
-	LOG_LEVEL_INFO
-	LOG_LEVEL_WARN
-	LOG_LEVEL_ERROR
-	LOG_LEVEL_FATAL
-)
+	EVENT_SERVER_STARTED    = "server_started"
+	EVENT_REQUEST_RECEIVED  = "[STRING NOT ASSIGNED]"
+	EVENT_BACKEND_SELECTING = "backend_selecting"
+	EVENT_BACKEND_SELECTED  = "backend_selected"
+	EVENT_BACKEND_SKIPPING  = "backend_skipping"
 
-const (
-	EVENT_SERVER_STARTED = iota
-	EVENT_REQUEST_RECEIVED
-	EVENT_BACKEND_SELECTED
-	EVENT_REQUEST_COMPLETED
+	EVENT_REQUEST_COMPLETED = "[STRING NOT ASSIGNED]"
 	EVENT_BACKEND_HEALTH_CHANGED
 	EVENT_BACKEND_ADDED
 	EVENT_BACKEND_REMOVED
-	EVENT_PROXY_ERROR_GENERIC
-	EVENT_PROXY_ERROR_STARTUP
+	EVENT_PROXY_ERROR
+	EVENT_PROXY_ERROR_STARTUP   = "proxy_error_startup"
+	EVENT_PROXY_STARTING        = "proxy_starting"
+	EVENT_PROXY_STOPPING        = "proxy_stopping"
+	EVENT_PROXY_RETURNING_503   = "proxy_returning_503"
+	EVENT_SERVER_ERROR          = "server_error"
+	EVENT_SERVER_ERROR_STARTUP  = "server_error_startup"
+	EVENT_CHAOS_EXITING         = "chaos_exiting"
+	EVENT_CHAOS_KILLING_BACKEND = "chaos_killing_backend"
+	EVENT_CHAOS_KILLED_BACKEND  = "chaos_killed_backend"
+	EVENT_CHAOS_FAILED_TO_KILL  = "chaos_listener_failed_to_kill"
+	EVENT_HEALTHCHECKER_ERROR   = "healthchecker_error"
+)
+
+const (
+	REASON_LISTEN_FAILED                 = "listen_failed"
+	REASON_PORTS_EXHAUSTED               = "ports_exhausted"
+	REASON_BACKEND_DEAD                  = "backend_dead"
+	REASON_DUMMY_URL_INVALID             = "dummy_url_invalid"
+	REASON_ALL_BACKENDS_DEAD             = "all_backends_dead"
+	REASON_LISTENER_ALREADY_CLOSED       = "listener_already_closed"
+	REASON_FAILED_TO_MARSHAL_TO_JSON     = "failed_to_marshal_to_json"
+	REASON_FAILED_TO_WRITE               = "failed_to_write"
+	REASON_STOPPING_NORMALLY             = "stopping_normally"
+	REASON_STOPPING_DUE_TO_ERROR         = "stopping_due_to_error"
+	REASON_REQUEST_FAILED                = "request_failed"
+	REASON_FAILED_TO_CLOSE_RESPONSE_BODY = "failed_to_close_response_body"
 )
